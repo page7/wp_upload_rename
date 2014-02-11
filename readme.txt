67ae3248f4fe16739a698b20c8df8f21082aa886
page7 <zhounan0120@gmail.com>
2014-02-12 00:48:20 +0800

    frist version 0.1.0
    Signed-off-by: page7 <zhounan0120@gmail.com>

---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Plugin Name ===
+Contributors: Nolan
+Donate link: http://www.nolanchou.com/wp_upload_rename
+Tags: upload, admin, images, image, media, file, rename, filename, short url, random, date, number
+Requires at least: 3.0
+Tested up to: 3.8.1
+Stable tag: 0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Change upload filename to random characters / random numbers / date / other by yourself.
+
+== Description ==
+
+Sometimes we forgot change a available name for our file. So the plugin can help you auto change your upload file.
+I will add a filetype option of setting in next version.
+And a rename popup before the file upload if you change setting in future, default value is post_name.
+
+
+== Installation ==
+
+There are many ways to install this plugin, e.g:
+
+1. Upload compressed (zip) plugin using WordPress plugin uploader.
+2. Directly install from WordPress.org directory
+3. Upload manually uncompressed plugin file using FTP.
+
+== Frequently Asked Questions ==
+
+[See official plugin support here](http://nolanchou.com/wp_upload_rename).
+
+== Screenshots ==
+
+
+== Changelog ==
+
+= 0.1 =
+* First release
+
+
+== Upgrade Notice ==
+
+= 0.1 =
+* First release
---
+++ wp_upload_rename.php
@@ -0,0 +1,162 @@
+<?php
+/*
+Plugin Name: wp_upload_rename
+Plugin URI: http://www.nolanchou.com/wp_upload_rename/
+Description: Rename upload file by random chars / numbers / date / other.
+Version: 1.0
+Author: Nolan Chou　
+Author URI: http://www.nolanchou.com/
+License: GUN v2
+*/
+
+
+
+if (!class_exists('wp_upload_rename'))
+{
+class wp_upload_rename
+{
+
+    protected $defaultOpts = array('mode'=>'char', 'length'=>5, 'param'=>'');
+
+
+    // construct
+    public function __construct()
+    {
+        if (is_admin())
+        {
+            $plugin = plugin_basename(__FILE__);
+            add_action('admin_init', array(&$this, 'register'));
+            add_action('admin_menu', array(&$this, 'menu'));
+            add_filter('plugin_action_links_'.$plugin, array(&$this, 'action_links'));
+            add_filter('wp_handle_upload_prefilter', array(&$this,'rename'));
+        }
+    }
+
+
+    // register setting
+    public function register()
+    {
+        register_setting('wp_upload_rename_setting', 'wp_upload_rename_options');
+    }
+
+
+    // Admin menu
+    public function menu()
+    {
+        add_options_page('Upload Rename', 'Upload Rename', 'administrator', 'wp_upload_rename', array(&$this, 'setting'));
+    }
+
+
+    // rename
+    public function rename($file)
+    {
+        $wp_filetype = wp_check_filetype_and_ext($file['tmp_name'], $file['name'], false);
+        extract($wp_filetype);
+        if (!$ext)
+            $ext = ltrim(strrchr($file['name'], '.'), '.');
+
+        $options = $this->option();
+        $newname = $this -> _name($options['mode'], (int)$options['length'], (string)$options['param']).'.'.$ext;
+        $file['name'] = str_replace('%file%', substr($file['name'], 0, -(strlen($ext)+1)), $newname);
+
+        return $file;
+    }
+
+
+
+    // get new name
+    protected function _name($mode, $length=5, $param='')
+    {
+        switch ($mode)
+        {
+            case 'char':
+                $chars = empty($param) ? 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' : (string)$param;
+                $chars = str_shuffle($chars);
+                $str = substr($chars, 0, $length);
+                break;
+            case 'num':
+                $str = sprintf("%0{$length}d", rand(0, str_repeat('9', $length)));
+                break;
+            case 'date':
+                $format = empty($param) ? 'Ymd_His' : $param;
+                $str = date($format);
+                break;
+            case 'diy':
+                if (empty($param))
+                    return $this -> _name('char', 5);
+
+                preg_match_all('/%((file|date|char|num)(\|[^%]+)?)%/', $param, $m);
+                foreach ($m[0] as $v)
+                {
+                    $args = explode('|', trim($v, '%'));
+                    if (in_array($args[0], array('char', 'num')))
+                        $rp = $this -> _name($args[0], empty($args[1]) ? 5 : (int)$args[1]);
+                    else if ( $args[0] == 'date' )
+                        $rp = $this -> _name($args[0], 0, empty($args[1]) ? '' : (int)$args[1]);
+                    else
+                        continue;
+
+                    $param = str_replace($v, $rp, $param);
+                }
+                $str = $param;
+                break;
+            default:
+                $str = '';
+        }
+        return $str;
+    }
+
+
+
+    // Setting options page.
+    public function setting()
+    {
+        if (!current_user_can('manage_options'))
+        {
+            wp_die($this->__('You do not have sufficient permissions to access this page.'));
+            return;
+        }
+
+        include(dirname(__FILE__).'/options.php');
+    }
+
+
+
+    // get options
+    function option($key='')
+    {
+        $option = get_option('wp_upload_rename_options') ? get_option('wp_upload_rename_options') : array();
+        $option = array_merge($this->defaultOpts, $option);
+        if ($key)
+            $return = $option[$key];
+        else
+            $return = $option;
+
+        return $return;
+    }
+
+
+
+    //Language
+    public function __($key) {
+        return __($key, 'wp_upload_rename');
+    }
+
+
+
+}
+}
+
+add_action('init', 'wp_upload_rename_init');
+function wp_upload_rename_init()
+{
+	if (class_exists('wp_upload_rename'))
+    {
+		new wp_upload_rename();
+	}
+}
+
+//register_uninstall_hook(__FILE__, array('wp_upload_rename', 'uninstall'));
+
+
+?>
